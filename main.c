e9cbbbc8c5a3ba144dbd93b819ec0e2e44b0fdfe
oscar <oscar@VPN200>
2014-03-20 12:34:41 +0800

    interface updated.

--- main.c
+++ main.c
@@ -37,6 +37,9 @@ static inline void deset_color_attr(int i) {
   attroff(dattr[i]);
 }
 
+static const char *strategy = "default";
+static char delay_str[100] = "0.5 sec";
+
 static void draw_game(char *ext_msg) {
   clear();
   move(0, 0);
@@ -64,6 +67,7 @@ static void draw_game(char *ext_msg) {
         deset_color_attr(k);
       }
   move(19, 0);
+  printw("Strategy: %s, Delay: %s\n", strategy, delay_str);
   refresh();
 }
 
@@ -132,34 +136,87 @@ static void move_game(int action) {
   color_set(N_BLUE, NULL); attron(A_BOLD);
   printw("%5d", maze[new_x][new_y]);
   color_set(0, NULL); attroff(A_BOLD);
-  move(19, 0);
+  move(20, 0);
 }
 
 static void finish_game(bool ok) {
   if (ok) {
-    // draw_game(" You win.\nPress `q' to quit, otherwise replay.\n");
-    move(19, 0);
+    move(20, 0);
     printw("You win.\nPress `q' to quit, `c' to replay.\n");
     refresh();
   } else {
-    // draw_game(" You lose.\nPress `q' to quit, otherwise replay.\n");
-    move(19, 0);
+    move(20, 0);
     printw("You lose.\nPress `q' to quit, `c' to replay.\n");
     refresh();
   }
 }
 
-int main(int argc, char **argv) {
-  // TODO read arguments
-  
-  if (argc > 1) {
-    if (strcmp(argv[1], "-m") == 0)
-      trans = user_input;
-    else if (strcmp(argv[1], "-rr") == 0)
-      trans = roundrobin;
-    else if (strcmp(argv[1], "-rr2") == 0)
-      trans = roundrobin2;
+static inline void help_msg() {
+  fprintf(stderr,
+"Usage:\n"
+"  2048 [-s strategy] [-d seconds] [-w] | [-h]\n"
+"  -s chose strategy [default, roundrobin, roundrobin2, manual_input]\n"
+"  -d display delay time (in seconds) in range [0, 10.0], default is 0.5\n"
+"  -w wait for user press a key to continue next move (only in automatic mode)\n"
+"  -h print help messages\n"
+  );
+}
+
+static int delay_usec = 500000;
+static bool wait_ch = false;
+
+void process_args(int argc, char **argv) {
+  extern char *optarg;
+  int result; double sec;
+  int getopt(int, char *const[], const char*);
+  while ((result = getopt(argc, argv, "s:d:wh")) != -1) {
+    switch (result) {
+     case 's':
+      if (strcmp(optarg, "default") == 0) {
+        strategy = "default";
+        trans = next_move;
+      } else if (strcmp(optarg, "roundrobin") == 0) {
+        strategy = "roundrobin";
+        trans = roundrobin;
+      } else if (strcmp(optarg, "roundrobin2") == 0) {
+        strategy = "roundrobin2";
+        trans = roundrobin2;
+      } else if (strcmp(optarg, "manual_input") == 0) {
+        strategy = "maual_input";
+        trans = user_input;
+      } else {
+        fprintf(stderr, "Error: unsupported strategy name!\n\n");
+        help_msg();
+        exit(1);
+      }
+      break;
+     case 'd':
+      sscanf(optarg, "%lf", &sec);
+      if (sec < 0 || sec > 10) {
+        fprintf(stderr, "Error: display delay time must be in range [0, 10.0]!\n\n");
+        help_msg();
+        exit(1);
+      } else {
+        sprintf(delay_str, "%g sec", sec);
+        delay_usec = sec * 1e6;
+      }
+      break;
+     case 'w':
+      strcpy(delay_str, "wait input");
+      wait_ch = true;
+      break;
+     case 'h':
+      help_msg();
+      exit(1);
+    }
   }
+  if (trans == user_input) {
+    strcpy(delay_str, "(NULL)");
+  }
+}
+
+int main(int argc, char **argv) {
+  process_args(argc, argv);
 
   initscr();
   cbreak();
@@ -184,14 +241,18 @@ int main(int argc, char **argv) {
     init_game();
     bool ok = true;
     while (!check_game()) {
-      //getch();
-      usleep(500000);
+      if (trans != user_input) {
+        if (wait_ch)
+          getch();
+        else
+          usleep(delay_usec);
+      }
       memcpy(maze_copy, maze, sizeof maze);
-      nodelay(stdscr, false);   // may request input then
       move_game(trans(maze_copy));
       nodelay(stdscr, true);
       while (getch() != ERR)
         ;
+      nodelay(stdscr, false);   // may request input then
       if (loss_game()) {
         ok = false;
         break;
